bcdcfc68b3fb8c9ac3bc323a5bd48812e8171828
milkytheway <milkytheway@example.com>
2025-08-03 13:51:24 +0800

    Initial commit

---
+++ BSP/led/inc/bsp_led.h
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * @file bsp_led.h
+ * 
+ * @par dependencies 
+ * - stdio.h
+ * - stdint.h
+ * 
+ * @author ll | XXXXX Dept. | EternalChip XXXXX
+ * 
+ * @brief Provide the HAL APIs of AHT21 and corresponding opetions.
+ * 
+ * Processing flow:
+ * 
+ * call directly.
+ * 
+ * @version V1.0 2025-7-19
+ *
+ * @note 1 tab == 4 spaces!
+ * 
+ *****************************************************************************/
+
+
+#ifndef __BSP_LED_H__
+#define __BSP_LED_H__
+
+//******************************** Includes *********************************//
+
+#include <stdint.h>               
+#include <stdio.h>
+
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+
+
+//******************************** Includes *********************************//
+
+//******************************** Defines **********************************//
+/* function return status                  */
+typedef enum
+{
+  LED_OK                = 0,         /* Operation completed successfully.  */
+  LED_ERROR             = 1,         /* Run-time error without case matched*/
+  LED_ERRORTIMEOUT      = 2,         /* Operation failed with timeout      */
+  LED_ERRORRESOURCE     = 3,         /* Resource not available.            */
+  LED_ERRORPARAMETER    = 4,         /* Parameter error.                   */
+  LED_ERRORNOMEMORY     = 5,         /* Out of memory.                     */
+  LED_ERRORISR          = 6,         /* Not allowed in ISR context         */
+  LED_RESERVED          = 0x7FFFFFFF /* Reserved                           */
+} LED_status_t;
+
+typedef enum
+{
+  LED_ON                = 0,         	/*      				LED on 							  */
+  LED_OFF               = 1,         	/*      				LED off							  */
+	LED_TOGGLE						= 2,          /*      				LED toggle		  		  */
+	LED_BLINK							= 3           /*      				LED BLINK		  		  	*/
+} LED_operation_t;
+
+//******************************** Defines **********************************//
+
+
+//******************************** Declaring ********************************//
+
+/**
+ * @brief Instantiates the bsp_led_handler_t target.
+ * 
+ * Steps:
+ *  1. Adds Core interfaces into bsp_led_driver instance target.
+ *  2. Adds OS interfaces into bsp_led_driver instance target.
+ *  3. Adds timebase interfaces into bsp_led_driver instance target.
+ *  
+ * @param[in] self        : Pointer to the target of handler.
+ * @param[in] os_delay    : Pointer to the os_delay_interface.
+ * @param[in] os_queue    : Pointer to the os_queue_interface.
+ * @param[in] os_critical : Pointer to the os_critical_interface.
+ * @param[in] os_thread   : Pointer to the os_thread_interface.
+ * @param[in] time_base   : Pointer to the time_base_interface.
+ * 
+ * @return led_handler_status_t : Status of the function.
+ * 
+ * */
+LED_status_t led_on_off(LED_operation_t led_operation);
+
+//******************************** Declaring ********************************//
+
+
+#endif // End of __BSP_LED_H__
+
---
+++ BSP/led/src/bsp_led.c
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * Copyright (C) 2024 EternalChip, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * @file bsp_led.c
+ * 
+ * @par dependencies 
+ * - bsp_led.h
+ * - stdio.h
+ * - stdint.h
+ * 
+ * @author ll | XXXXX Dept. | EternalChip XXXXX
+ * 
+ * @brief Provide the HAL APIs of AHT21 and corresponding opetions.
+ * 
+ * Processing flow:
+ * 
+ * call directly.
+ * 
+ * @version V1.0 2025-7-19
+ *
+ * @note 1 tab == 4 spaces!
+ * 
+ *****************************************************************************/
+
+#include "bsp_led.h"
+
+
+
+/**
+ * @brief Detect key press event during the life time of key_scan
+ * 
+ * Steps:
+ *  1, detect key pressed or not, within a shrot period
+ *  2, if pressed, 
+ * 
+ * @param[in] uint32_t *key_value : an adress to store the status of the key
+ * @param[out] array_vaild_number : The member number of this array.
+ * 
+ * @return  LED_status_t.
+ * 
+ * */
+ LED_status_t led_on_off(LED_operation_t led_operation)
+ {
+	 LED_status_t func_status = LED_ERROR;
+	 
+	 if(LED_ON == led_operation)
+	 {
+		 // turn on the LED
+		 HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+		 // A check mechanism should be added here to determine GPIO operation
+		 func_status = LED_OK;
+	 }
+	 
+	 if(LED_OFF == led_operation)
+	 {
+		 // turn off the LED
+		 HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+		 func_status = LED_OK;
+	 }
+	 
+	 if(LED_TOGGLE == led_operation)
+	 {
+		 // toggle the LED
+		 HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+		 func_status = LED_OK;
+	 }
+	 
+	 if(LED_BLINK == led_operation)
+	 {
+		 //blink LED 3 times
+		 for(int i=0; i<6; i++)
+		 {
+			 HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+			 
+			 HAL_Delay(500);
+		 }
+	 }
+	 
+	 return func_status;
+ }
+
